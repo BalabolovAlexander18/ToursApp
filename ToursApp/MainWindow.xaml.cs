a5c7b749b86f9d4866fbd3e8c0e0633d057519ba
oleg- <oleg-@DESKTOP-LDQHJ84>
2024-12-10 12:53:08 +0800

    Добавьте файлы проекта.

---
+++ ToursApp/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new LoginPage());
+            Manager.MainFrame = MainFrame;
+
+            //ImportTours();
+        }
+
+        private void ImportTours()
+        {
+            var fileData = File.ReadAllLines(@"C:\Users\oleg-\OneDrive\Рабочий стол\Разработка ПМ\УП.01.01\import до\Туры2.txt");
+            var images = Directory.GetFiles(@"C:\Users\oleg-\OneDrive\Рабочий стол\Разработка ПМ\УП.01.01\import до\Туры фото");
+
+            foreach (var line in fileData)
+            {
+                var data  = line.Split('\t');
+
+                var tempTour = new Tour
+                {
+                    Name = data[0].Replace("\"", ""),
+                    TicketCount = int.Parse(data[2]),
+                    Price = Decimal.Parse(data[3]),
+                    isActual = (data[4] == "0") ? false : true,
+                };
+
+                foreach (var tourType in data[5].Replace("\"", "").Split(new String[] { ", "}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    var currentType = ToursBaseEntities2.GetContext().Types.ToList().FirstOrDefault(p => p.Name == tourType);
+                    if (currentType != null)
+                        tempTour.Types.Add(currentType);
+                }
+
+                try
+                {
+                    tempTour.ImagePreview = File.ReadAllBytes(images.FirstOrDefault(p => p.Contains(tempTour.Name)));
+                }
+                catch (Exception ex) 
+                {
+                        MessageBox.Show(ex.Message);
+                }
+
+                ToursBaseEntities2.GetContext().Tours.Add(tempTour);
+                ToursBaseEntities2.GetContext().SaveChanges();
+            }
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFrame.CanGoBack)
+            {
+                btnBack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btnBack.Visibility = Visibility.Hidden;
+            }
+        }
+    }
+}
---
+++ ToursApp/WorkersPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ToursApp;
+
+namespace ToursApp
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkersPage.xaml
+    /// </summary>
+    public partial class WorkersPage : Page
+    {
+        public WorkersPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var AdminUser = ToursBaseEntities2.GetContext().Users.FirstOrDefault(p => p.Type == "Administrator");
+            var ManagerUser = ToursBaseEntities2.GetContext().Users.FirstOrDefault(p => p.Type == "Manager");
+            if (textBoxLogin.Text == AdminUser.Login && PasBox.Password == Encryption.DecryptString(AdminUser.Password, AdminUser.KeyPassword))
+            {
+                Manager.MainFrame.Navigate(new HotelsPage());
+            }
+            else if (textBoxLogin.Text == ManagerUser.Login && PasBox.Password == Encryption.DecryptString(ManagerUser.Password, ManagerUser.KeyPassword))
+            {
+                Manager.MainFrame.Navigate(new HotelsPage());
+            }
+            else
+            {
+                MessageBox.Show("Не удалось войти. Проверьте логин и пароль!");
+            }
+        }
+    }
+
+    public static class Encryption
+    {
+        public static string EncryptString(string plainText, string keyString)
+        {
+            byte[] key = Convert.FromBase64String(keyString);
+            byte[] iv = new byte[16]; // Vector initialization, usually 16 bytes for AES
+            Array.Clear(iv, 0, iv.Length); // Инициализируем IV нулями (для примера; в реальности лучше генерировать случайный)
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText);
+                        }
+                    }
+                    return Convert.ToBase64String(msEncrypt.ToArray());
+                }
+            }
+        }
+
+        public static string DecryptString(string cipherText, string keyString)
+        {
+            byte[] key = Convert.FromBase64String(keyString);
+            byte[] iv = new byte[16];
+            Array.Clear(iv, 0, iv.Length);
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(Convert.FromBase64String(cipherText)))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            return srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
